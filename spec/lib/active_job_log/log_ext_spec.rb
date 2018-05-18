209ae8885fd14c44fcda78d11e86d28cc7e47e39
Leandro Segovia <ldlsegovia@gmail.com>
2018-05-18 15:03:21 -0300

    tests(): missing specs for job extension

---
+++ spec/lib/active_job_log/log_ext_spec.rb
@@ -0,0 +1,84 @@
+require 'rails_helper'
+
+RSpec.describe ActiveJobLog::LogExt do
+  def remove_job_class
+    Object.send(:remove_const, :TestJob)
+  rescue
+    nil
+  end
+
+  def perform_now
+    TestJob.perform_now(*job_params)
+  rescue
+    nil
+  end
+
+  let(:job_params) do
+    [
+      "p1",
+      "p2"
+    ]
+  end
+
+  before { remove_job_class }
+
+  it { expect(ActiveJobLog::Job.count).to eq(0) }
+
+  context "with successful job" do
+    before do
+      class TestJob < ActiveJob::Base
+        def perform(param1, param2)
+          "success with #{param1} and #{param2}"
+        end
+      end
+
+      perform_now
+      @job = ActiveJobLog::Job.last
+    end
+
+    it { expect(ActiveJobLog::Job.count).to eq(1) }
+    it { expect(@job.job_id).not_to be_nil }
+    it { expect(@job.params).to eq(job_params) }
+    it { expect(@job.status).to eq(:finished) }
+    it { expect(@job.job_class).to eq("TestJob") }
+    it { expect(@job.queue_name).to eq("default") }
+    it { expect(@job.error).to be_nil }
+    it { expect(@job.stack_trace).to eq([]) }
+    it { expect(@job.queued_at).to be_nil }
+    it { expect(@job.started_at).not_to be_nil }
+    it { expect(@job.ended_at).not_to be_nil }
+    it { expect(@job.queued_duration).to be_nil }
+    it { expect(@job.execution_duration).not_to be_nil }
+    it { expect(@job.total_duration).not_to be_nil }
+    it { expect(@job.executions).to eq(0) }
+  end
+
+  context "with failed job" do
+    before do
+      class TestJob < ActiveJob::Base
+        def perform(_param1, _param2)
+          raise "error"
+        end
+      end
+
+      perform_now
+      @job = ActiveJobLog::Job.last
+    end
+
+    it { expect(ActiveJobLog::Job.count).to eq(1) }
+    it { expect(@job.job_id).not_to be_nil }
+    it { expect(@job.params).to eq(job_params) }
+    it { expect(@job.status).to eq(:failed) }
+    it { expect(@job.job_class).to eq("TestJob") }
+    it { expect(@job.queue_name).to eq("default") }
+    it { expect(@job.error).to eq("error") }
+    it { expect(@job.stack_trace.any?).to eq(true) }
+    it { expect(@job.queued_at).to be_nil }
+    it { expect(@job.started_at).not_to be_nil }
+    it { expect(@job.ended_at).not_to be_nil }
+    it { expect(@job.queued_duration).to be_nil }
+    it { expect(@job.execution_duration).not_to be_nil }
+    it { expect(@job.total_duration).not_to be_nil }
+    it { expect(@job.executions).to eq(0) }
+  end
+end
--- spec/models/active_job_log/job_spec.rb
+++ spec/models/active_job_log/job_spec.rb
@@ -1,189 +1,187 @@
 require 'rails_helper'
 
-module ActiveJobLog
-  RSpec.describe Job, type: :model do
-    it "has a valid factory" do
-      expect(create(:active_job_log_job)).to be_valid
+RSpec.describe ActiveJobLog::Job, type: :model do
+  it "has a valid factory" do
+    expect(create(:active_job_log_job)).to be_valid
+  end
+
+  describe "Validations" do
+    it { is_expected.to enumerize(:status).in(:queued, :pending, :finished, :failed) }
+  end
+
+  describe "#params" do
+    let(:job) { create(:active_job_log_job) }
+    let(:params) { [1, 2] }
+
+    it { expect(job.params).to eq([]) }
+    it { expect { job.params = params }.to change(job, :params).from([]).to(params) }
+  end
+
+  describe "#stack_trace" do
+    let(:job) { create(:active_job_log_job) }
+    let(:stack) { [1, 2] }
+
+    it { expect(job.stack_trace).to eq([]) }
+    it { expect { job.stack_trace = stack }.to change(job, :stack_trace).from([]).to(stack) }
+  end
+
+  describe "#set_queued_duration" do
+    let(:job) { build(:active_job_log_job, queued_at: queued_at, started_at: started_at) }
+
+    context "with defined started_at and queued_at" do
+      let(:queued_at) { started_at - 2.days }
+      let(:started_at) { DateTime.current }
+
+      it { expect { job.save! }.to change(job, :queued_duration).from(nil).to(172800) }
     end
 
-    describe "Validations" do
-      it { is_expected.to enumerize(:status).in(:queued, :pending, :finished, :failed) }
+    context "with nil started_at" do
+      let(:queued_at) { DateTime.current }
+      let(:started_at) { nil }
+
+      it { expect { job.save! }.not_to change(job, :queued_duration) }
     end
 
-    describe "#params" do
-      let(:job) { create(:active_job_log_job) }
-      let(:params) { [1, 2] }
+    context "with nil queued_at" do
+      let(:queued_at) { nil }
+      let(:started_at) { DateTime.current }
 
-      it { expect(job.params).to eq([]) }
-      it { expect { job.params = params }.to change(job, :params).from([]).to(params) }
+      it { expect { job.save! }.not_to change(job, :queued_duration) }
     end
+  end
 
-    describe "#stack_trace" do
-      let(:job) { create(:active_job_log_job) }
-      let(:stack) { [1, 2] }
+  describe "#set_execution_duration" do
+    let(:job) { build(:active_job_log_job, started_at: started_at, ended_at: ended_at) }
 
-      it { expect(job.stack_trace).to eq([]) }
-      it { expect { job.stack_trace = stack }.to change(job, :stack_trace).from([]).to(stack) }
-    end
-
-    describe "#set_queued_duration" do
-      let(:job) { build(:active_job_log_job, queued_at: queued_at, started_at: started_at) }
-
-      context "with defined started_at and queued_at" do
-        let(:queued_at) { started_at - 2.days }
-        let(:started_at) { DateTime.current }
-
-        it { expect { job.save! }.to change(job, :queued_duration).from(nil).to(172800) }
-      end
-
-      context "with nil started_at" do
-        let(:queued_at) { DateTime.current }
-        let(:started_at) { nil }
-
-        it { expect { job.save! }.not_to change(job, :queued_duration) }
-      end
-
-      context "with nil queued_at" do
-        let(:queued_at) { nil }
-        let(:started_at) { DateTime.current }
-
-        it { expect { job.save! }.not_to change(job, :queued_duration) }
-      end
-    end
-
-    describe "#set_execution_duration" do
-      let(:job) { build(:active_job_log_job, started_at: started_at, ended_at: ended_at) }
-
-      context "with defined ended_at and started_at" do
-        let(:started_at) { ended_at - 2.days }
-        let(:ended_at) { DateTime.current }
-
-        it { expect { job.save! }.to change(job, :execution_duration).from(nil).to(172800) }
-      end
-
-      context "with nil ended_at" do
-        let(:started_at) { DateTime.current }
-        let(:ended_at) { nil }
-
-        it { expect { job.save! }.not_to change(job, :execution_duration) }
-      end
-
-      context "with nil started_at" do
-        let(:started_at) { nil }
-        let(:ended_at) { DateTime.current }
-
-        it { expect { job.save! }.not_to change(job, :execution_duration) }
-      end
-    end
-
-    describe "#set_total_duration" do
-      let(:job) do
-        build(:active_job_log_job, queued_at: queued_at, started_at: started_at, ended_at: ended_at)
-      end
-
-      let(:queued_at) { ended_at - 3.days }
+    context "with defined ended_at and started_at" do
       let(:started_at) { ended_at - 2.days }
       let(:ended_at) { DateTime.current }
 
-      it { expect { job.save! }.to change(job, :total_duration).from(nil).to(259200) }
-
-      context "with undefined queued_at" do
-        let(:queued_at) { nil }
-
-        it { expect { job.save! }.to change(job, :total_duration).from(nil).to(172800) }
-      end
-
-      context "with undefined started_at" do
-        let(:started_at) { nil }
-
-        it { expect { job.save! }.to change(job, :total_duration).from(nil).to(259200) }
-      end
-
-      context "with undefined ended_at" do
-        let(:queued_at) { DateTime.current - 3.days }
-        let(:started_at) { DateTime.current - 2.days }
-        let(:ended_at) { nil }
-
-        it { expect { job.save! }.not_to change(job, :total_duration) }
-      end
-
-      context "with undefined queued_at and started_at" do
-        let(:queued_at) { nil }
-        let(:started_at) { nil }
-        let(:ended_at) { DateTime.current }
-
-        it { expect { job.save! }.not_to change(job, :total_duration) }
-      end
+      it { expect { job.save! }.to change(job, :execution_duration).from(nil).to(172800) }
     end
 
-    describe "#update_job!" do
-      let(:job_id) { "x" }
-      let(:status) { :queued }
-      let(:perform) { described_class.update_job!(job_id, status, params) }
+    context "with nil ended_at" do
+      let(:started_at) { DateTime.current }
+      let(:ended_at) { nil }
 
-      let(:params) do
-        {
-          params: [1],
-          stack_trace: [2],
-          error: "error",
-          job_class: "MyJob"
-        }
-      end
+      it { expect { job.save! }.not_to change(job, :execution_duration) }
+    end
 
-      it { expect { perform }.to change(ActiveJobLog::Job, :count).from(0).to(1) }
+    context "with nil started_at" do
+      let(:started_at) { nil }
+      let(:ended_at) { DateTime.current }
 
-      context "with existent job" do
-        let!(:job) { create(:active_job_log_job, job_id: job_id) }
+      it { expect { job.save! }.not_to change(job, :execution_duration) }
+    end
+  end
 
-        it { expect { perform }.not_to change(ActiveJobLog::Job, :count) }
-      end
+  describe "#set_total_duration" do
+    let(:job) do
+      build(:active_job_log_job, queued_at: queued_at, started_at: started_at, ended_at: ended_at)
+    end
 
-      context "with queued status" do
-        before { @job = perform }
+    let(:queued_at) { ended_at - 3.days }
+    let(:started_at) { ended_at - 2.days }
+    let(:ended_at) { DateTime.current }
 
-        it { expect(@job.job_id).to eq(job_id) }
-        it { expect(@job.error).to eq("error") }
-        it { expect(@job.params).to eq([1]) }
-        it { expect(@job.stack_trace).to eq([2]) }
-        it { expect(@job.job_class).to eq("MyJob") }
-        it { expect(@job.status).to eq(:queued) }
-        it { expect(@job.queued_at).not_to be_nil }
-        it { expect(@job.started_at).to be_nil }
-        it { expect(@job.ended_at).to be_nil }
-      end
+    it { expect { job.save! }.to change(job, :total_duration).from(nil).to(259200) }
 
-      context "with pending status" do
-        let(:status) { :pending }
+    context "with undefined queued_at" do
+      let(:queued_at) { nil }
 
-        before { @job = perform }
+      it { expect { job.save! }.to change(job, :total_duration).from(nil).to(172800) }
+    end
 
-        it { expect(@job.status).to eq(:pending) }
-        it { expect(@job.queued_at).to be_nil }
-        it { expect(@job.started_at).not_to be_nil }
-        it { expect(@job.ended_at).to be_nil }
-      end
+    context "with undefined started_at" do
+      let(:started_at) { nil }
 
-      context "with finished status" do
-        let(:status) { :finished }
+      it { expect { job.save! }.to change(job, :total_duration).from(nil).to(259200) }
+    end
 
-        before { @job = perform }
+    context "with undefined ended_at" do
+      let(:queued_at) { DateTime.current - 3.days }
+      let(:started_at) { DateTime.current - 2.days }
+      let(:ended_at) { nil }
 
-        it { expect(@job.status).to eq(:finished) }
-        it { expect(@job.queued_at).to be_nil }
-        it { expect(@job.started_at).to be_nil }
-        it { expect(@job.ended_at).not_to be_nil }
-      end
+      it { expect { job.save! }.not_to change(job, :total_duration) }
+    end
 
-      context "with failed status" do
-        let(:status) { :failed }
+    context "with undefined queued_at and started_at" do
+      let(:queued_at) { nil }
+      let(:started_at) { nil }
+      let(:ended_at) { DateTime.current }
 
-        before { @job = perform }
+      it { expect { job.save! }.not_to change(job, :total_duration) }
+    end
+  end
 
-        it { expect(@job.status).to eq(:failed) }
-        it { expect(@job.queued_at).to be_nil }
-        it { expect(@job.started_at).to be_nil }
-        it { expect(@job.ended_at).not_to be_nil }
-      end
+  describe "#update_job!" do
+    let(:job_id) { "x" }
+    let(:status) { :queued }
+    let(:perform) { described_class.update_job!(job_id, status, params) }
+
+    let(:params) do
+      {
+        params: [1],
+        stack_trace: [2],
+        error: "error",
+        job_class: "MyJob"
+      }
+    end
+
+    it { expect { perform }.to change(ActiveJobLog::Job, :count).from(0).to(1) }
+
+    context "with existent job" do
+      let!(:job) { create(:active_job_log_job, job_id: job_id) }
+
+      it { expect { perform }.not_to change(ActiveJobLog::Job, :count) }
+    end
+
+    context "with queued status" do
+      before { @job = perform }
+
+      it { expect(@job.job_id).to eq(job_id) }
+      it { expect(@job.error).to eq("error") }
+      it { expect(@job.params).to eq([1]) }
+      it { expect(@job.stack_trace).to eq([2]) }
+      it { expect(@job.job_class).to eq("MyJob") }
+      it { expect(@job.status).to eq(:queued) }
+      it { expect(@job.queued_at).not_to be_nil }
+      it { expect(@job.started_at).to be_nil }
+      it { expect(@job.ended_at).to be_nil }
+    end
+
+    context "with pending status" do
+      let(:status) { :pending }
+
+      before { @job = perform }
+
+      it { expect(@job.status).to eq(:pending) }
+      it { expect(@job.queued_at).to be_nil }
+      it { expect(@job.started_at).not_to be_nil }
+      it { expect(@job.ended_at).to be_nil }
+    end
+
+    context "with finished status" do
+      let(:status) { :finished }
+
+      before { @job = perform }
+
+      it { expect(@job.status).to eq(:finished) }
+      it { expect(@job.queued_at).to be_nil }
+      it { expect(@job.started_at).to be_nil }
+      it { expect(@job.ended_at).not_to be_nil }
+    end
+
+    context "with failed status" do
+      let(:status) { :failed }
+
+      before { @job = perform }
+
+      it { expect(@job.status).to eq(:failed) }
+      it { expect(@job.queued_at).to be_nil }
+      it { expect(@job.started_at).to be_nil }
+      it { expect(@job.ended_at).not_to be_nil }
     end
   end
 end
